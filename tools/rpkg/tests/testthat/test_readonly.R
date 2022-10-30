cc625cdecabf447fdf9bd3d0d6133255921a39ac
Kirill Müller <krlmlr+r@mailbox.org>
2022-10-30 05:38:28 +0100

    Need prefix for callr

--- tools/rpkg/tests/testthat/test_readonly.R
+++ tools/rpkg/tests/testthat/test_readonly.R
@@ -4,11 +4,11 @@ test_that("read_only flag and shutdown works as expected", {
   # 1st: create a db and write some tables
 
   callr::r(function(dbdir) {
-    con <- dbConnect(duckdb(), dbdir, read_only = FALSE) # FALSE is the default
+    con <- DBI::dbConnect(duckdb::duckdb(), dbdir, read_only = FALSE) # FALSE is the default
     print(con)
-    res <- dbWriteTable(con, "iris", iris)
-    dbDisconnect(con)
-    duckdb_shutdown(con@driver)
+    res <- DBI::dbWriteTable(con, "iris", iris)
+    DBI::dbDisconnect(con)
+    duckdb::duckdb_shutdown(con@driver)
   }, args = list(dbdir))
 
 
@@ -25,9 +25,9 @@ test_that("read_only flag and shutdown works as expected", {
 
   # con is still alive
   callr::r(function(dbdir) {
-    con <- dbConnect(duckdb(), dbdir, read_only = TRUE)
-    res <- dbReadTable(con, "iris")
-    dbDisconnect(con, shutdown = TRUE)
+    con <- DBI::dbConnect(duckdb::duckdb(), dbdir, read_only = TRUE)
+    res <- DBI::dbReadTable(con, "iris")
+    DBI::dbDisconnect(con, shutdown = TRUE)
   }, args = list(dbdir))
 
   # shut down one of them again
@@ -40,9 +40,9 @@ test_that("read_only flag and shutdown works as expected", {
   # now we can get write access again
   # TODO shutdown
   callr::r(function(dbdir) {
-    con <- dbConnect(duckdb(), dbdir, read_only = FALSE) # FALSE is the default
-    res <- dbWriteTable(con, "iris2", iris)
-    dbDisconnect(con)
+    con <- DBI::dbConnect(duckdb::duckdb(), dbdir, read_only = FALSE) # FALSE is the default
+    res <- DBI::dbWriteTable(con, "iris2", iris)
+    DBI::dbDisconnect(con)
   }, args = list(dbdir))
 
   expect_true(TRUE)
